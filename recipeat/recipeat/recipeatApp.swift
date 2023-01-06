45c280c46805e5acebb833365f1b35f8e2ad27d5
sinah <sina_haghighi@sfu.ca>
2023-01-06 00:07:58 -0800

    the beginning of a very cool app

---
+++ recipeat/recipeat/recipeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeatApp.swift
+//  recipeat
+//
+//  Created by Sina Haghighi on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct recipeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
